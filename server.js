1d75cb0c992ae0ba894728f020774d9327e658a7
peazzoni1 <peazzoni@yahoo.com>
2016-05-19 14:18:31 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express    = require('express');
+var app        = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+       res.sendfile(__dirname + '/index.html');
+   });
+
+//start server
+app.listen(port);
+console.log('The party started at ' + port);
